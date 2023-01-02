77e5c554c02f15e262ab809174649894844fbbe0
Abraham Afework <Abrahamafework7@gmail.com>
2023-01-02 22:31:49 +0200

    importing 'this' to print the Zen of pyhton

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,14 @@
+#include"list.h"
+/**
+ *check_cycle -function checks if a singly linked list has a cycle in it.
+ *
+ *@list:
+ * 
+ * Return : 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+
+	return (1)
+
+}
