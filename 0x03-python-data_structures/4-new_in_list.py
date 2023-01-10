0814f716690061f60b8bc6f6acf7dc61a5bee149
Abraham Afework <Abrahamafework7@gmail.com>
2023-01-10 20:24:40 +0200

    Write a function that prints all integers of a list, in reverse order.

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, new_element) :
-   
-       if idx > 0 and idx < len(my_list):
-             new_list = my_list[0:]
-             new_list.pop(idx)
-             new_list.insert(idx,9)
-             return new_list
-       else:
-             return None
+def new_in_list(my_list, idx, new_element):
+    if my_list is not None:
+        new_list = my_list[0:]
+        if idx >= 0 and idx < len(my_list):
+            new_list[idx] = new_element
+            return new_list
+        else:
+            return my_list
+    else:
+        return None
