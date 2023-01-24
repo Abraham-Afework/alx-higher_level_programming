eb33da7f21ac0c56579710e4ca08c3441b9997f8
Abraham Afework <Abrahamafework7@gmail.com>
2023-01-24 15:00:17 +0200

    unction that prints an integer with {:d}.format().

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    try:
+        if (value / 1 == value):
+            print("{:d}".format(value))
+            return True
+    except Exception:
+        return False
