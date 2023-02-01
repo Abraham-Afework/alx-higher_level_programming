621ab66cd1bdcc65fa0d79e8188287454ec313eb
Abraham Afework <Abrahamafework7@gmail.com>
2023-02-01 16:23:34 +0200

    magic string

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
-#!/urs/bin/python3
-def magic_string():
-    magic_string.count += 1
-    return ",".join(["BestSchool"]*magic_string.count)
-magic_string.count = 0
+#!/usr/bin/python3
+def magic_string(count = [0]):
+    count[0] += 1
+    return ",".join(["BestSchool"]*count[0])
