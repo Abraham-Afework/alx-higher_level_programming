e78ff4da2c435111c9645529a2bfb34ff6f46c6c
Abraham Afework <Abrahamafework7@gmail.com>
2023-02-15 09:31:41 +0200

    adding test cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,11 +2,30 @@
 import unittest
 from models.base import Base
 
+
 class BaseTest(unittest.TestCase):
-	def test_base(self):
-		first = Base()
-		second = Base()
-		self.assertEqual(first.id, second.id-1)
-	
+    def test_base(self):
+        first = Base()
+        second = Base()
+        self.assertEqual(first.id, second.id-1)
+
+    def test_base_saves_id(self):
+        first = Base(89)
+        self.assertEqual(first.id, 89)
+
+    def test_base_to_json_string_none(self):
+        first = Base.to_json_string(None)
+        self.assertEqual(first, "[]")
+        self.assertEqual(second, "[8]")
+
+    def test_base_to_json_string_empty(self):
+        second = Base.to_json_string([])
+        self.assertEqual(second, "[]")
+
+    def test_base_to_json_string_id(self):
+        second = Base.to_json_string([{"id": 12}])
+        self.assertEqual(second, "[]")
+
+
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -2,27 +2,27 @@
 import unittest
 from models.rectangle import Rectangle
 
+
 class TestRectangle(unittest.TestCase):
 
-	def test_no_args(self):
-		with self.assertRaises(TypeError):
-			Rectangle()
+    def test_no_args(self):
+        with self.assertRaises(TypeError):
+            Rectangle()
 
-	def test_one_args(self):
-		with self.assertRaises(TypeError):
-			Rectangle(1)
+    def test_one_args(self):
+        with self.assertRaises(TypeError):
+            Rectangle(1)
 
-	def test_two_args(self):
-		first = Rectangle(1, 2)
-		second = Rectangle(1, 2)
-		self.assertEqual(first.id, second.id - 1)
+    def test_two_args(self):
+        first = Rectangle(1, 2)
+        second = Rectangle(1, 2)
+        self.assertEqual(first.id, second.id - 1)
 
-	def test_three_args(self):
-		first = Rectangle(1, 2, 3)
-		second = Rectangle(1, 2, 3)
-		self.assertEqual(first.id, second.id - 1)
+    def test_three_args(self):
+        first = Rectangle(1, 2, 3)
+        second = Rectangle(1, 2, 3)
+        self.assertEqual(first.id, second.id - 1)
 
-	def test_morethan_five_args(self):
-		with self.assertRaises(TypeError):
-			Rectangle(1, 2, 3, 4, 5, 6)
-		
+    def test_morethan_five_args(self):
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, 3, 4, 5, 6)
