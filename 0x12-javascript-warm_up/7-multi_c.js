90b28a285b4717c85a8d7dc2f5d13f90574b61f9
Abraham Afework <Abrahamafework7@gmail.com>
2023-04-12 22:08:47 +0200

    7-multi_c.js amended

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-const myNumber = 1;
 let i = 0;
-const firstArg = process.argv[2];
-console.log(typeof(firstArg - myNumber));
-if (typeof(firstArg * myNumber) == 'number') {
-  while (i < firstArg){
-  console.log('C is fun');
-  i++;
+let firstArg = process.argv[2];
+firstArg = parseInt(firstArg);
+if (!isNaN(firstArg)) {
+  while (i < firstArg) {
+    console.log('C is fun');
+    i++;
   }
-
 } else { console.log('Missing number of occurrences'); }
