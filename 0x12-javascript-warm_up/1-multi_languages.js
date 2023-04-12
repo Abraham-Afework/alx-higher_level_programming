e8ee0d664b546d9c734db48994c768d656629719
Abraham Afework <Abrahamafework7@gmail.com>
2023-04-12 15:41:10 +0200

    script that prints 3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,7 @@
-#!/usr/bin/nodejs
-const myVar = ["C is fun", "Python is cool", "JavaScript is amazing"]
-let i = 0
-while (i < 3) 
-{
-	console.log(myVar[i])
-	i++
+#!/usr/bin/node
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+while (i < 3) {
+  console.log(myVar[i]);
+  i++;
 }
