6db23112f09f58648e07c0d421dc5a3580bdedce
Abraham Afework <Abrahamafework7@gmail.com>
2023-04-12 18:08:41 +0200

    prints 3 lines

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,6 @@
-#!/usr/bin/nodejs
-const myVar = "JavaScript is amazing"
-	console.log(myVar)
+#!/usr/bin/node
+const myNumber = 1;
+const firstArg = process.argv[2];
+if (typeof (firstArg * myNumber) === 'number') {
+  console.log('My Number: ' + firstArg);
+} else { console.log('Not a number'); }
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,7 @@
-#!/usr/bin/nodejs
-const myVar = "JavaScript is amazing"
-	console.log(myVar)
+#!/usr/bin/node
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+while (i < 3) {
+  console.log(myVar[i]);
+  i++;
+}
