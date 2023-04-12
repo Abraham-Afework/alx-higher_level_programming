690a3cfec2b9b861d4839af6b71031e48c1da8cd
Abraham Afework <Abrahamafework7@gmail.com>
2023-04-12 18:18:51 +0200

    script that prints x times “C is fun”

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,3 +1,12 @@
-#!/usr/bin/nodejs
-const myVar = "JavaScript is amazing"
-	console.log(myVar)
+#!/usr/bin/node
+const myNumber = 1;
+let i = 0;
+const firstArg = process.argv[2];
+console.log(typeof(firstArg - myNumber));
+if (typeof(firstArg * myNumber) == 'number') {
+  while (i < firstArg){
+  console.log('C is fun');
+  i++;
+  }
+
+} else { console.log('Missing number of occurrences'); }
