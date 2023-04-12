8d485d10c6e0367fb9af6bb823a1aa32d2bdf069
Abraham Afework <Abrahamafework7@gmail.com>
2023-04-12 16:44:26 +0200

    prints the first argument passed to it:

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,3 +1,8 @@
-#!/usr/bin/nodejs
-const myVar = "JavaScript is amazing"
-	console.log(myVar)
+#!/usr/bin/node
+
+const count = process.argv.length - 2;
+if (count === 0) {
+  console.log('No argument');
+} else if (count >= 1) {
+  console.log(process.argv[2]);
+}
