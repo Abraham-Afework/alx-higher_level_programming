3be857530786296a83d08c44c8044071cd87280d
Abraham Afework <Abrahamafework7@gmail.com>
2023-04-12 21:52:02 +0200

    amended

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-const myNumber = 1;
-const firstArg = process.argv[2];
-if (typeof (firstArg * myNumber) === 'number') {
-  console.log('My Number: ' + firstArg);
+
+let firstArg = process.argv[2];
+
+firstArg = parseInt(firstArg);
+
+if (!isNaN(firstArg)) {
+  console.log('My number: ' + firstArg);
 } else { console.log('Not a number'); }
