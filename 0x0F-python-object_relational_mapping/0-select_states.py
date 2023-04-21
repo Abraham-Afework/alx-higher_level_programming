2e6cefbb8de7494271682dca1784d1591ba2218b
Abraham Afework <Abrahamafework7@gmail.com>
2023-04-21 13:15:31 +0200

    script that lists all states from the database hbtn_0e_0_usa:

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" script that lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+from sys import argv
+
+
+def RunScript():
+    """ Function that uses to run the script """
+
+    config = {
+        'host': 'localhost',
+        'port': 3306,
+        'user': argv[1],
+        'passwd': argv[2],
+        'db': argv[3],
+        'charset': 'utf8'
+        }
+
+    db = MySQLdb.connect(**config)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    RunScript()
