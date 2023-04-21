1db85b2f8b3b4ef72393c7e6e4ef60a6dc7ffbda
Abraham Afework <Abrahamafework7@gmail.com>
2023-04-21 14:10:40 +0200

    script should take 4 arguments: mysql username, mysql password, database name and state name searched

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" script that lists all states with a name starting with N (upper N)\
+        from the database hbtn_0e_0_usa:"""
+
+import MySQLdb
+from sys import argv
+
+
+def RunScript():
+    """ Function that uses to run the script """
+
+    config = {
+        'host': 'localhost',
+        'port': 3306,
+        'user': argv[1],
+        'passwd': argv[2],
+        'db': argv[3],
+        'charset': 'utf8'
+        }
+    query = "SELECT * FROM states WHERE NAME = '" + argv[4] + "'"
+    db = MySQLdb.connect(**config)
+    cur = db.cursor()
+    cur.execute(query)
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    RunScript()
